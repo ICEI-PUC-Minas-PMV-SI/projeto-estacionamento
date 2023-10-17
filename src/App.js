236850b3e880e726ff4695b556f39d7dbd2e0f1e
guilhermefialho <92562186+guilhermefialho@users.noreply.github.com>
2023-10-17 17:49:46 -0300

    Create RedefinirSenha

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Routes, Route as ReactRoute } from 'react-router'; 
 
 import Login from './components/Login';
-import HomePage from './components/HomePage';
 import Cadastro from './components/Cadastro';
+import RedefinirSenha from './components/RedefinirSenha';
 
 function App() {
   return (
     <Router>
       <Routes>
         <ReactRoute path="/"     element={<Login />} />
-        <ReactRoute path="/home" element={<HomePage />} />
+        <ReactRoute path="/redefinirsenha" element={<RedefinirSenha />} />
         <ReactRoute path="/cadastro" element={<Cadastro />} />
       </Routes>
     </Router>
