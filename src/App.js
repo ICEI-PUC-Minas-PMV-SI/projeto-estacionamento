83dc68372386797c386e2420d3b84599f0fa353f
livmorais <liviasantosmorais@gmail.com>
2023-10-17 19:26:06 -0300

    att app

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import { Routes, Route as ReactRoute } from 'react-router';
 
 import Login from './components/Login';
 import Cadastro from './components/Cadastro';
-<<<<<<< HEAD
 import Vagas from './components/Vagas';
-=======
 import RedefinirSenha from './components/RedefinirSenha';
->>>>>>> 236850b3e880e726ff4695b556f39d7dbd2e0f1e
+
 
 function App() {
   return (
